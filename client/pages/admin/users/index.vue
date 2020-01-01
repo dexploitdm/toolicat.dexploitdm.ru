4ffcc717c05ede5b615ccac1eb4c138d28a6b303
Дмитрий Паршаков <knutdmit@yandex.ru>
2020-01-01 19:02:07 +0500

    cat

---
+++ client/pages/admin/users/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Пользователи</h4>
+                        <p class="card-category">Created using Montserrat Font Family</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        layout: 'admin',
+        name: "userIndex"
+    }
+</script>
+
+<style scoped>
+
+</style>
