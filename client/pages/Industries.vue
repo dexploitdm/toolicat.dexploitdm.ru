5ac79a33c497548d7954269af05ac78f333c2856
Дмитрий Паршаков <knutdmit@yandex.ru>
2020-01-05 17:21:20 +0500

    add pages /TOOL-2

---
+++ client/pages/Industries.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="page content">
+        <div class="title-box">
+            <h1 class="title-h1">Отрасли</h1>
+        </div>
+        <div class="box">
+            Industries
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Industries"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/pages/promotional.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="page content">
+        <div class="title-box">
+            <h1 class="title-h1">Промокоды</h1>
+        </div>
+        <div class="box">
+            promotional
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "promotional"
+    }
+</script>
+
+<style scoped>
+
+</style>
