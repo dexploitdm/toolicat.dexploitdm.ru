ceb777424e78f2ae3395cc1d6cb2138b0f432cac
Дмитрий Паршаков <knutdmit@yandex.ru>
2020-01-12 15:07:38 +0500

    sammlungen

---
+++ client/components/home/Sammlungen.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="sammlungen">
+        <div class="sammlungen-schicht box">
+            <div class="c-header-box">
+                <h2 class="title-h2">Подборки</h2>
+            </div>
+        </div>
+        <sammlungen-blatt
+            title="Топ 50"
+            icon="top"
+            color="rgb(3, 83, 164)"
+        />
+        <sammlungen-blatt
+            title="Бесплатные"
+            icon="free"
+            color="rgb(19, 167, 19)"
+        />
+        <sammlungen-blatt
+            title="Сервисы для it-стартапов"
+            icon="it"
+            color="rgb(51, 54, 60)"
+        />
+    </div>
+</template>
+
+<script>
+    import SammlungenBlatt from "~/components/home/SammlungenBlatt.vue";
+    export default {
+        name: "Sammlungen",
+        components: {
+            SammlungenBlatt
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/components/home/SammlungenBlatt.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="sammlungen-blatt box">
+        <div class="sammlungen-box" :style="'background-color:' + color ">
+            <div class="sammlungen-block">
+                <div :class="'sammlungen-icon ' + icon"></div>
+                <div class="sammlungen-title">
+                    {{title}}
+                </div>
+            </div>
+        </div>
+        <div class="sammlungen-werkzeuge">
+            <div class="sammlungen-werkzeuge-icons">
+                <a href="#"><img src="/images/services/logo/599d6d2090c61.png"></a>
+                <a href="#"><img src="/images/services/logo/599f31496ce4b.png"></a>
+                <a href="#"><img src="/images/services/logo/599f2ae1e3f3c.png"></a>
+                <a href="#"><img src="/images/services/logo/59a550e015e0f.png"></a>
+            </div>
+            <div class="sammlungen-werkzeuge-text">
+                + еще 46 сервисов
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['title', 'icon', 'color'],
+        name: "SammlungenBlatt"
+    }
+</script>
+
+<style scoped>
+
+</style>
